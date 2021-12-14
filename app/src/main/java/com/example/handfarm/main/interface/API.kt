fbf136885efa932877cb72c38d6c4c57ac73e0b0
yangbogyu <didqhrb1@gmail.com>
2021-12-14 22:51:49 +0900

    market test

--- app/src/main/java/com/example/handfarm/main/interface/API.kt
+++ app/src/main/java/com/example/handfarm/main/interface/API.kt
@@ -5,14 +5,18 @@ import com.example.handfarm.main.data.Login_PUT
 import com.example.handfarm.main.data.Market_Data
 import com.example.handfarm.main.data.UserInfo
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.PUT
+import retrofit2.http.*
 
 interface API {
     @GET("market")
+    @Headers("accept: application/json",
+        "content-type: application/json")
     fun getMarketInfo(): Call<Market_Data>
 
     @PUT("logins")
-    fun getUserInfo(@Body userData : Login_PUT): Call<Login_Data>
+    @Headers("accept: application/json",
+        "content-type: application/json")
+    fun LoginPUT(
+        @Body userData : Login_PUT
+    ): Call<Login_Data>
 }
